45e85ae18d0fbcf378010a19ce413641816dc863
damiansliwa <damian.sliwa.90@gmail.com>
2018-09-24 19:53:31 +0200

    Install rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+
+  # Turn the deprecation warnings into errors to give a full backtrace
+  config.raise_errors_for_deprecations!
+
+end
